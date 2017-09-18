69405d292d4564537492ad46430dc31e699411f5
rita <rita_liu@friday.tw>
2017-09-18 17:02:21 +0800

    update guesbook.php

--- 0915/GuestBook.php
+++ 0915/GuestBook.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>30 Days to be RD</title>
+  <title>30 Days To Be RD</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -49,6 +49,7 @@
               <!-- User image -->
               <li class="user-header">
                 <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <p>Alexander Pierce</p>
               </li>
               <!-- Menu Footer-->
               <li class="user-footer">
@@ -69,11 +70,11 @@
       <div class="box-header">
         <i class="fa fa-comments-o"></i>
 
-        <h3 class="box-title">GuestBook</h3>
+        <h3 class="box-title">留言板</h3>
 
         <div class="box-tools pull-right" data-toggle="tooltip" title="Status">
           <div class="btn-group" data-toggle="btn-toggle">
-            <button type="button" class="btn btn-default btn-sm active"><i class="fa fa-square text-green"> 新增留言</i>
+            <button type="button" class="btn btn-default"><i class="fa fa-square text-green"> 新增留言</i>
             </button>
           </div>
         </div>
@@ -85,68 +86,61 @@
 
           <p class="message">
             <a href="#" class="name">
-              <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 2:15</small>
               Mike Doe
             </a>
+            <span class="pull-right">
+            <a href="#" class="label label label-success">回覆留言</a>
+            <a href="#" class="label label label-danger">刪除</a>
+            </span>
+            <small class="text-muted"><i class="fa fa-clock-o"></i> 2:15</small>
+            <div>
             I would like to meet you to discuss the latest news about
             the arrival of the new theme. They say it is going to be one the
             best themes on the market
+            </div>
           </p>
           <div class="attachment">
-            <h4>Attachments:</h4>
-
-            <p class="filename">
-              Theme-thumbnail-image.jpg
-            </p>
-
-            <div class="pull-right">
-              <button type="button" class="btn btn-primary btn-sm btn-flat">刪除</button>
-            </div>
+            <a href="#" class="name">
+              <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 2:15</small>
+              Mike Doe
+            </a>
+            <p>Theme-thumbnail-image.jpg</p>
+            <div class="pull-right"><a href="#" class="label label-danger">刪除</a></div>       
           </div>
           <div class="attachment">
-            <h4>Attachments:</h4>
-
-            <p class="filename">
-              Theme-thumbnail-image.jpg
-            </p>
-
-            <div class="pull-right">
-              <button type="button" class="btn btn-primary btn-sm btn-flat">刪除</button>
-            </div>
+            <a href="#" class="name">
+              <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 2:15</small>
+              Mike Doe
+            </a>
+            <p>Theme-thumbnail-image.jpg</p>
+            <div class="pull-right"><a href="#" class="label label-danger">刪除</a></div>
           </div>
           <!-- /.attachment -->
         </div>
         <!-- /.item -->
         <!-- chat item -->
         <div class="item">
-          <img src="dist/img/user3-128x128.jpg" alt="user image" class="offline">
+          <img src="dist/img/user4-128x128.jpg" alt="user image" class="online">
 
           <p class="message">
             <a href="#" class="name">
-              <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 5:15</small>
-              Alexander Pierce
+              Mike Doe
             </a>
+            <span class="pull-right">
+            <a href="#" class="label label label-success">回覆留言</a>
+            <a href="#" class="label label label-danger">刪除</a>
+            </span>
+            <small class="text-muted"><i class="fa fa-clock-o"></i> 2:15</small>
+            <div>
             I would like to meet you to discuss the latest news about
             the arrival of the new theme. They say it is going to be one the
             best themes on the market
+            </div>
           </p>
+          <!-- /.attachment -->
         </div>
         <!-- /.item -->
-        <!-- chat item -->
-        <div class="item">
-          <img src="dist/img/user2-160x160.jpg" alt="user image" class="offline">
 
-          <p class="message">
-            <a href="#" class="name">
-              <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 5:30</small>
-              Susan Doe
-            </a>
-            I would like to meet you to discuss the latest news about
-            the arrival of the new theme. They say it is going to be one the
-            best themes on the market
-          </p>
-        </div>
-        <!-- /.item -->
       </div>
       <!-- /.chat -->
     </div>
